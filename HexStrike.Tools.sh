2985b68802baf9608bb58daf843437e411ad6291
VhaTer <VhaTer@users.noreply.github.com>
2025-08-21 08:26:40 +0000

    Update HexStrike.Tools.sh
    Replace all instances of \\033 with \033 in your variable definitions.

--- HexStrike.Tools.sh
+++ HexStrike.Tools.sh
@@ -4,17 +4,17 @@
 # Supports multiple Linux distributions with verified download links
 # Version 4.0 - Complete coverage of all 150+ HexStrike AI tools
 
-RED='\\033[0;31m'
-GREEN='\\033[0;32m'
-YELLOW='\\033[1;33m'
-BLUE='\\033[0;34m'
-MAGENTA='\\033[0;35m'
-CYAN='\\033[0;36m'
-WHITE='\\033[1;37m'
-ORANGE='\\033[0;33m'
-PURPLE='\\033[0;35m'
-BOLD='\\033[1m'
-NC='\\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+ORANGE='\033[0;33m'
+PURPLE='\033[0;35m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
 
 # Enhanced Banner
 echo -e "${CYAN}"
